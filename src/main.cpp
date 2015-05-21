e588148aa5dbffc8126e8f1b3bc7bce4436a4ead
Stephen Seo <seo.disparate@gmail.com>
2015-05-21 17:15:01 -0400

    Made ParserTest not require any input

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,56 @@
 #include "AnotherDangParser.hpp"
 
 #include <iostream>
+#include <cstdlib>
+#include <cstring>
 
-int main(int argc, char** argv)
+int main()
 {
+    std::cout << "Simulating input with input:\n\t./ParserTest -daop Option_O Option_P\n";
+
+    int argc = 4;
+    char* argv[5];
+
+    argv[0] = (char*)std::malloc(sizeof(char) * 13);
+    argv[1] = (char*)std::malloc(sizeof(char) * 6);
+    argv[2] = (char*)std::malloc(sizeof(char) * 9);
+    argv[3] = (char*)std::malloc(sizeof(char) * 9);
+    argv[4] = (char*)std::malloc(sizeof(char) * 1);
+
+    argv[0][0] = '.';
+    argv[0][1] = '/';
+    argv[0][2] = 'P';
+    argv[0][3] = 'a';
+    argv[0][4] = 'r';
+    argv[0][5] = 's';
+    argv[0][6] = 'e';
+    argv[0][7] = 'r';
+    argv[0][8] = 'T';
+    argv[0][9] = 'e';
+    argv[0][10] = 's';
+    argv[0][11] = 't';
+    argv[0][12] = '\0';
+
+    argv[1][0] = '-';
+    argv[1][1] = 'd';
+    argv[1][2] = 'a';
+    argv[1][3] = 'o';
+    argv[1][4] = 'p';
+    argv[1][5] = '\0';
+
+    argv[2][0] = 'O';
+    argv[2][1] = 'p';
+    argv[2][2] = 't';
+    argv[2][3] = 'i';
+    argv[2][4] = 'o';
+    argv[2][5] = 'n';
+    argv[2][6] = '_';
+    argv[2][7] = 'O';
+    argv[2][8] = '\0';
+
+    std::strcpy(argv[3], "Option_P\0");
+    std::strcpy(argv[4], "\0");
+
     AnotherDangParser parser;
     parser.addFlag("d", [] () {
         std::cout << "Derp.\n";
@@ -21,4 +68,9 @@ int main(int argc, char** argv)
     });
 
     parser.parse(argc, argv);
+
+    for(int i = 0; i < 5; ++i)
+    {
+        free(argv[i]);
+    }
 }
